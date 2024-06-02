1701b29b7d3880787123e82ffaf518ab9b1fb3f9
taishin-miyamoto <taishin.miyamoto@dena.com>
2024-06-02 20:35:12 +0900

    feat: プロジェクトの作成

---
+++ FlipClock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlipClock
+//
+//  Created by Taishin Miyamoto on 2024/06/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlipClock/FlipClockApp.swift
@@ -0,0 +1,19 @@
+//
+//  FlipClockApp.swift
+//  FlipClock
+//
+//  Created by Taishin Miyamoto on 2024/06/02.
+//
+// フリップ時計の作り方
+// https://www.youtube.com/watch?v=Lekoc7QS-K4
+
+import SwiftUI
+
+@main
+struct FlipClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
